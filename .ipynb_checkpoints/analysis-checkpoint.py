8755415a1ddb118cd40ee59a0ed89f87792ca77d
ripR0ACH <morrisonizach@gmail.com>
2023-08-13 02:40:13 -0400

    idek atp

--- .ipynb_checkpoints/analysis-checkpoint.py
+++ .ipynb_checkpoints/analysis-checkpoint.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append("../../lhillber/brownian/src")
 from time_series import CollectionTDMS as ctdms
 from acoustic_entrainment import mic_response
+from multiprocessing.dummy import Pool as ThreadPool
 
 class System():
 
@@ -68,7 +69,7 @@ class System():
         """
         return self.__data
 
-    def set_data(self, df) -> None:
+    def set_data(self, df = [], ind = 0) -> None:
         """
         
         set_data sets the data collections for every data file provided for the system.
@@ -76,15 +77,24 @@ class System():
         :return: None.
         
         """
-        self.__data = np.empty((len(df), ), dtype=object)
-        for d in range(len(df)):
-            self.__data[d] = ctdms(df[d])
+        if len(df) != 0:
+            self.__data = np.empty((len(df), ), dtype=object)
+            for d in range(len(df)):
+                self.__data[d] = ctdms(df[d])
+                if self.get_name()[:3] == "mic":
+                    self.__data[d].set_collection("Y")
+                    self.__data[d].apply("correct", response = mic_response, recollect = True)
+                else:
+                    self.__data[d].set_collection("X")
+                    self.__data[d].apply("calibrate", cal = -1, inplace = True)
+        else:
+            self.__data[ind] = ctdms(self.get_df()[ind])
             if self.get_name()[:3] == "mic":
-                self.__data[d].set_collection("Y")
-                self.__data[d].apply("correct", response = mic_response, recollect = True)
+                self.__data[ind].set_collection("Y")
+                self.__data[ind].apply("correct", response = mic_response, recollect = True)
             else:
-                self.__data[d].set_collection("X")
-                self.__data[d].apply("calibrate", cal = -1, inplace = True)
+                self.__data[ind].set_collection("X")
+                self.__data[ind].apply("calibrate", cal = -1, inplace = True)
         return None
 
     def get_power(self) -> int:
@@ -165,16 +175,16 @@ class System():
         """"""
         return self.__SNR_freq_range
 
-    def local_detrend(self, col = [], tmin = None, tmax = None, inplace = False) -> None:
+    def local_detrend(self, col = [], index = 0, tmin = None, tmax = None, inplace = False) -> None:
         """"""
         if col == []:
-            for d in self.get_data():
-                for c in d.collection:
-                    t, x = c.time_gate(tmin = tmin, tmax = tmax)
-                    x_bar = np.mean(x)
-                    m, b = np.polyfit(t, x, 1)
-                    if inplace:
-                        c.x = c.x - (m * c.t) - b
+            d = self.get_data()[index]
+            for c in d.collection:
+                t, x = c.time_gate(tmin = tmin, tmax = tmax)
+                x_bar = np.mean(x)
+                m, b = np.polyfit(t, x, 1)
+                if inplace:
+                    c.x = c.x - (m * c.t) - b
         else:
             for c in col.collection:
                 t, x = c.time_gate(tmin = tmin, tmax = tmax)
@@ -186,33 +196,29 @@ class System():
 
     def reset_SNR_at_cutoff(self) -> None:
         """"""
-        self.__SNR_at_cutoff = np.array([])
+        self.__SNR_at_cutoff = []
         return None
-    def calc_SNR_at_cutoff(self, f = 0, bins = False) -> np.array([]):
+    def calc_SNR_at_cutoff(self, f = 0, bins = False, lowpass = False) -> np.array([]):
         """"""
         if f == 0:
             f = self.get_SNR_freq_cutoff()
         snr = []
-        for dat in self.get_data()[:]:
-            if self.get_name()[:3] == "mic":
-                dat.set_collection("Y")
-                dat.apply("correct", response = mic_response, recollect = True)  
-                # had to remove mic correction because it was causing a  
-                # ringing to appear in signals after lowpass or bin average
+        for i in range(len(self.__data)):
+            if lowpass and bins:
+                self.__data[i].apply("lowpass", cutoff = f, inplace = True)
+                self.__data[i].apply("bin_average", Npts = int(self.__data[i].r / (2 * f)), inplace = True)
+            elif bins:
+                self.__data[i].apply("bin_average", Npts = int(self.__data[i].r / (2 * f)), inplace = True)
             else:
-                dat.set_collection("X")
-                dat.apply("calibrate", cal = -1, inplace = True)
+                self.__data[i].apply("lowpass", cutoff = f, inplace = True)
+            self.local_detrend(col = self.__data[i], tmin = 0, tmax = 3.5e-4, inplace = True)
             peaks = np.array([])
             rms = np.array([])
-            if bins:
-                dat.apply("bin_average", Npts = int(dat.r / (2 * f)), inplace = True)
-            else:
-                dat.apply("lowpass", cutoff = f, inplace = True)
-            self.local_detrend(dat, 0, 4e-4, True)
-            for s in dat.collection[1:]:
+            for s in self.__data[i].collection[1:]:
                 peaks = np.append(peaks, np.abs(max(s.time_gate(tmin = 3.5e-4, tmax = 5e-4)[1])))
                 rms = np.append(rms, np.std(s.time_gate(tmin = 2e-4, tmax = 3.5e-4)[1]))
             snr.append(np.mean(peaks / rms))
+            self.set_data(ind = i)
         return snr
     def set_SNR_at_cutoff(self, snr) -> None:
         """"""
@@ -226,14 +232,14 @@ class System():
         """"""
         self.__SNR_vs_freq = []
         return None
-    def calc_SNR_vs_freq(self, freq = [0, 0], bins = False) -> None:
+    def calc_SNR_vs_freq(self, freq = [0, 0], bins = False, lowpass = False) -> None:
         """"""
         if freq[0] == 0 and freq[1] == 0:
             freq = self.get_SNR_freq_range()
         else:
             freq = np.linspace(freq[0], freq[1], self.get_SNR_resolution())
         for i in range(len(freq)):
-            self.__SNR_vs_freq.append(self.calc_SNR_at_cutoff(freq[i], bins))
+            self.__SNR_vs_freq.append(self.calc_SNR_at_cutoff(freq[i], bins, lowpass))
         self.__SNR_vs_freq = np.array(self.__SNR_vs_freq)
         return None
     def get_SNR_vs_freq(self) -> np.array([]):
